1ebc9e428948bcc5e375b5a0e2784138913d4a34
izumi-hyun <38989711+izumi-hyun@users.noreply.github.com>
2023-07-25 14:39:44 +0900

    Create 2438.cpp

---
+++ src/2438.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+	int N;
+	std::cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j <= i; j++)
+		{
+			std::cout << "*";
+		}
+		std::cout << std::endl;
+	}
+}
